5aab149a2dafc904f150dd0670748f36dec16f30
Stefan Douma <douma4@gmail.com>
2019-04-07 22:43:20 +0200

    Added to game engine

--- tests/GameEngineTest.py
+++ tests/GameEngineTest.py
@@ -18,6 +18,15 @@ class GameEngineTest(unittest.TestCase):
             if count == 10:
                 raise Exception("No difference in combination")
             count += 1;
+    def test_should_give_hint(self):
+        gameEngine = GameEngine();
+        self.assertEqual('++++', gameEngine.hint('ABCD', 'ABCD'));
+        self.assertEqual('+++ ', gameEngine.hint('ABCF', 'ABCD'));
+        self.assertEqual('--- ', gameEngine.hint('BCDE', 'ABCD'));
+    def test_end_reached(self):
+        gameEngine = GameEngine();
+        self.assertFalse(gameEngine.endReached(1));
+        self.assertTrue(gameEngine.endReached(8));
 
 class GameEngine:
     def getCombination(self):
@@ -26,6 +35,18 @@ class GameEngine:
         for x in range(0,4):
             combination += random.choice(randomList)
         return combination;
+    def hint(self, input, answer):
+        output = "";
+        for x in range(0,4):
+            if input[x] == answer[x]:
+                output += "+";
+            elif input[x] in answer:
+                output += "-";
+            else:
+                output += " ";
+        return output
+    def endReached(self, limit):
+        return limit == 8;
 
 if __name__ == '__main__':
     unittest.main()
